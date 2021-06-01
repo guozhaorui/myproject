51af9356e0e09aba1e660b3511520b0aa7df8050
guozhaorui <guozhaorui@126.com>
2021-06-02 00:04:43 +0800

    测试提交

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,10 +1,10 @@
 import com.test.TestApplication;
+import com.test.mq.MQSender;
 import com.test.service.TestService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -13,9 +13,19 @@ public class TestClass {
 
     @Autowired
     private TestService testService;
+    @Autowired
+    private MQSender mqSender;
+
     @Test
-    public void testList()  {
+    public void testList() {
         Object object = testService.query();
         System.out.println(object);
+    }
+
+    @Test
+    public  void testMq(){
+        for(int i=0;i<10;i++){
+            mqSender.sendSeckillMessage(String.valueOf(i));
+        }
     }
 }
