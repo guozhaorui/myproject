99b8e3be53b5f6233249a46577dc4abb19650b1b
郭钊锐 <guozhaorui@126.com>
2021-06-01 19:04:25 +0800

    测试

--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,17 @@ import com.test.service.TestService;
 @RequestMapping(value = "/test", produces = {"application/json;charset=utf-8"})
 public class TestController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     private TestService testService;
 
     @RequestMapping("/query")
     public void test() {
+        LOGGER.info("开始");
         Object object = testService.query();
         System.out.println(object);
+        LOGGER.info("结束");
     }
 
     @RequestMapping("/startAddPerson")
